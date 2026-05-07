e1376c470b307cef64ad8b7dbed19bd481b87461
Oscar KALOMBO <45149824+ososdis@users.noreply.github.com>
2026-05-07 12:43:44 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Work_CI_CD
-Repos pour le Test CI_CD
+En 40 minutes, faites les actions suivantes
+- Fork de ce repo
+- Clone en local
+- Creer une Branch avec votre prenom
+- Modifier le code suivant les instructions demandées et commit
+- Push sur votre fork
+- Pull Request
---
+++ main.py
@@ -0,0 +1,14 @@
+# Juste un script pour montrer que vous avez réussi ce petit test
+# sur la contribution dans un dépôt Github
+#
+
+# Retirer les commentaires ci-dessous et complétez ce script
+
+# nom = ???
+# prenom = ???
+# promo = ???
+#
+
+# print(f"Bonjour ! Je reponds au nom de {nom} {prenom}, promotion {promo}")
+
+print("Congrats")
